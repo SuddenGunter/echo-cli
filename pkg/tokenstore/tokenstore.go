baa09e443b51285f6f07b1aa362fe663c5e5b41d
Artem Kolomytsev <kolomytsev1996@gmail.com>
2019-07-05 02:05:11 +0300

    added read method to token store

--- pkg/tokenstore/tokenstore.go
+++ pkg/tokenstore/tokenstore.go
@@ -1,8 +1,15 @@
 package tokenstore
 
+import "github.com/pkg/errors"
+
 // TokenStore defines interface for auth token persistent storage
 type TokenStore interface {
 
-	// Save is main and only method of TokenStore that takes token string and save it somewhere
+	// Save takes token string and save it somewhere
 	Save(token string) error
+	// Read and return token from store
+	// Returns ErrTokenNotFound if token not found
+	Read() (string, error)
 }
+
+var ErrTokenNotFound = errors.New("Auth token not found")
