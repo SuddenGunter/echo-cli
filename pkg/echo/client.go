4655f8e2bbe8c6fb95dbf10d4aeb88016da702e6
Artem <kolomytsev1996@gmail.com>
2019-07-08 21:26:16 +0300

    ws client implemented

--- pkg/echo/client.go
+++ pkg/echo/client.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/pkg/errors"
 
@@ -13,25 +14,30 @@ import (
 type Client struct {
 	connectionString string
 	route            string
-	authToken        string
 	connection       *websocket.Conn
 	closed           chan struct{}
 	messageReceiver  chan string
 }
 
-func NewClient(connectionString string, route string, authToken string, messageReceiver chan string) *Client {
+// NewClient creates new client for websocket connection
+// example config: connectionString: "localhost:8080", route: "/ws"
+func NewClient(connectionString string, route string, messageReceiver chan string) *Client {
 	return &Client{
 		connectionString: connectionString,
 		route:            route,
-		authToken:        authToken,
 		messageReceiver:  messageReceiver,
 	}
 }
 
-func (client *Client) Connect() error {
+// Connect allows to establish websocket connection to configured server using user's auth token
+func (client *Client) Connect(authToken string) error {
+	if client.connection != nil {
+		return errors.WithStack(errors.New("Already connected"))
+	}
+
 	u := url.URL{Scheme: "ws", Host: client.connectionString, Path: client.route}
 	header := http.Header{}
-	header.Add("secretKey", client.authToken)
+	header.Add("secretKey", authToken)
 	conn, _, err := websocket.DefaultDialer.Dial(u.String(), header)
 	if err != nil {
 		return errors.Wrap(err, "Failed to establish websocket connection")
@@ -40,8 +46,18 @@ func (client *Client) Connect() error {
 	return nil
 }
 
+// SendMessage sends plain-text message over websocket channel to server
 func (client *Client) SendMessage(message string) error {
-	//close???
+	if client.connection == nil {
+		return errors.WithStack(errors.New("Not connected to any server"))
+	}
+
+	select {
+	case <-client.closed:
+		return errors.WithStack(errors.New("Connection closed"))
+	default:
+	}
+
 	err := client.connection.WriteMessage(websocket.TextMessage, []byte(message))
 	if err != nil {
 		return errors.Wrap(err, "Failed to save message by websocket connection")
@@ -52,8 +68,13 @@ func (client *Client) SendMessage(message string) error {
 // Listen start sending all received messages to configured messageReceiver
 func (client *Client) Listen() {
 	go func() {
-		defer close(client.closed)
 		for {
+			select {
+			case <-client.closed:
+				return
+			default:
+			}
+
 			_, message, err := client.connection.ReadMessage()
 			if err != nil {
 				log.Println("Failed to read message by websocket connection", err)
@@ -66,6 +87,10 @@ func (client *Client) Listen() {
 
 // Close sends websocket.CloseMessage to server and disconnects
 func (client *Client) Close() error {
+	close(client.closed)
+	//wait for readers/writers to finish receiving/sending messages
+	time.Sleep(time.Second)
+
 	err := client.connection.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 	if err != nil {
 		return errors.Wrap(err, "Failed to close websocket connection")
